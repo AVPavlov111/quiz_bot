28481040cd6205711960e535c6ee4f8c63e1260a
Artem Sergeev <aesergnsk@gmail.com>
2020-02-27 18:07:54 +0700

    вынес перехват кастомных команд

--- TrueQuizBot.WebApi/Dialogs/CancelAndHelpDialog.cs
+++ TrueQuizBot.WebApi/Dialogs/CancelAndHelpDialog.cs
@@ -31,21 +31,31 @@ namespace TrueQuizBot.WebApi.Dialogs
         {
             if (innerDc.Context.Activity.Type == ActivityTypes.Message)
             {
-                var text = innerDc.Context.Activity.Text.ToLowerInvariant();
+                var text = innerDc.Context.Activity.Text;
 
                 switch (text)
                 {
+                    
                     case "help":
+                    case "Help":
                     case "?":
                         var helpMessage = MessageFactory.Text(HelpMsgText, HelpMsgText, InputHints.ExpectingInput);
                         await innerDc.Context.SendActivityAsync(helpMessage, cancellationToken);
                         return new DialogTurnResult(DialogTurnStatus.Waiting);
 
                     case "cancel":
+                    case "Cancel":
                     case "quit":
+                    case "Quit":
                         var cancelMessage = MessageFactory.Text(CancelMsgText, CancelMsgText, InputHints.IgnoringInput);
                         await innerDc.Context.SendActivityAsync(cancelMessage, cancellationToken);
                         return await innerDc.CancelAllDialogsAsync(cancellationToken);
+                    case Constants.TrueLuckyTitle:
+                        return await innerDc.BeginDialogAsync(nameof(TrueLuckyDialog));
+                    case Constants.TrueTasksTitle:
+                        return await innerDc.BeginDialogAsync(nameof(QuizDialog));
+                    case Constants.TrueEmotionsTitle:
+                        return await innerDc.BeginDialogAsync(nameof(TrueEmotionsDialog));
                 }
             }
 
